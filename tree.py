8432a90c7f78ae47d672ba1cb6127f86ec28e316
Sagar Joshi <coderjoshi15@gmail.com>
2025-05-24 14:00:00 +0530

    commit2

--- tree.py
+++ tree.py
@@ -45,3 +45,56 @@ class BinaryOp:
         self.left = left
         self.op = op
         self.right = right
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from llvmlite import ir, binding
+
+class CodeGen:
+    def __init__(self):
+        self.module = ir.Module(name="jit_module")
+        self.builder = None
+        self.func = None
+
+    def compile(self, ast):
+        func_type = ir.FunctionType(ir.DoubleType(), [])
+        self.func = ir.Function(self.module, func_type, name="main")
+        block = self.func.append_basic_block(name="entry")
+        self.builder = ir.IRBuilder(block)
+        result = self.codegen(ast)
+        self.builder.ret(result)
+
+    def codegen(self, node):
+        if isinstance(node, Number):
+            return ir.Constant(ir.DoubleType(), node.value)
+        elif isinstance(node, BinaryOp):
+            l = self.codegen(node.left)
+            r = self.codegen(node.right)
+            if node.op == '+': return self.builder.fadd(l, r)
+            if node.op == '-': return self.builder.fsub(l, r)
+            if node.op == '*': return self.builder.fmul(l, r)
+            if node.op == '/': return self.builder.fdiv(l, r)
+        raise TypeError("Unknown node")
