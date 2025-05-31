c3101c9c3c94f9feda3e52f79d1f994c30f2c443
sagar1239joshi <gdg.geu.sagar.150904@gmail.com>
2025-05-31 12:47:52 +0530

    Create ast.py

---
+++ ast.py
@@ -0,0 +1,27 @@
+from llvmlite import ir, binding
+
+# Setup LLVM module
+module = ir.Module(name="jit_module")
+func_type = ir.FunctionType(ir.DoubleType(), [], False)
+function = ir.Function(module, func_type, name="main")
+block = function.append_basic_block(name="entry")
+builder = ir.IRBuilder(block)
+
+def compile_expr(expr):
+    if isinstance(expr, Number):
+        return ir.Constant(ir.DoubleType(), expr.value)
+    elif isinstance(expr, BinaryOp):
+        left_val = compile_expr(expr.left)
+        right_val = compile_expr(expr.right)
+        if expr.op == '+':
+            return builder.fadd(left_val, right_val)
+        elif expr.op == '-':
+            return builder.fsub(left_val, right_val)
+        elif expr.op == '*':
+            return builder.fmul(left_val, right_val)
+        elif expr.op == '/':
+            return builder.fdiv(left_val, right_val)
+        else:
+            raise ValueError(f"Unknown operator {expr.op}")
+    else:
+        raise TypeError("Unknown expression type")
